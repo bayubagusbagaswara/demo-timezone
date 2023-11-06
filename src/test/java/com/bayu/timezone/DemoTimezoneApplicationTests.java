252607b7d0303cecfd4e608e6948c25d404613f5
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-11-06 13:08:51 +0700

    Initial commit

---
+++ src/test/java/com/bayu/timezone/DemoTimezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.timezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTimezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
